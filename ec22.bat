f5e7f4c41302f03d24ab5b297bd32d10bc392693
Saswati2022 <100376474+Saswati2022@users.noreply.github.com>
2022-03-25 19:44:12 -0700

    Update ec22.bat

--- ec22.bat
+++ ec22.bat
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-0d3e7e2a2c45bfef1 --count 1 --instance-type t2.micro --key-name Image keypair --security-groups launch-wizard-5 --region us-east-2 
+aws ec2 run-instances --image-id ami-0d3e7e2a2c45bfef1 --count 1 --instance-type t2.micro --key-name newkey --security-groups launch-wizard-5 --region us-east-2 
